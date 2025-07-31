29800a73c321da88d460b1f0da26dcb6194d7b4f
James Logan <jlogan03@gmail.com>
2025-07-31 19:24:14 -0400

    add readme example and doc test

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 Python bindings for RustFFT, notably providing planned FFTs (meaning the bulk of the effort is cached and reusable)
 without platform restrictions and with permissive licensing.
 
+```python
+import numpy as np
+from rustfft import FftPlanner
+
+# Complex-valued input is consumed as output buffer
+n = 1024
+buffer = np.random.uniform(0.0, 1.0, n).astype(np.complex128)
+
+# Pre-planned FFT caches the initial setup
+fft = FftPlanner(n)
+
+# Run FFT repeatedly without re-initializing
+fft.process(buffer)
+fft.process(buffer)
+```
 
 # License
 
--- src/rustfft/__init__.py
+++ src/rustfft/__init__.py
@@ -16,22 +16,26 @@ from rustfft._core import (
 
 
 class FftPlanner:
+    """Prepared FFT plan for a given size, direction, and data type."""
+
     _inner: _FftPlannerF32 | _FftPlannerF64
     _dtype: DTypeLike
 
     def __init__(
         self,
         len: int,
-        direction: Literal["forward", "inverse"],
-        dtype: Literal["c32", "c64"] = "c64",
+        direction: Literal["forward", "inverse"] = "forward",
+        dtype: Literal["c64", "c128"] = "c128",
     ):
         """Prepare an FFT plan for a given size, direction, and data type.
 
         Args:
             len: Number of elements in the input
-            direction: Forward FFT or inverse
-            dtype: Whether to use 32-bit floats or 64-bit floats for each part of a complex number.
-                   Defaults to "c64".
+            direction: Forward FFT or inverse.
+                       Defaults to "forward".
+            dtype: Whether to use 32-bit floats (c64) or 64-bit floats (c128)
+                   for each part of a complex number.
+                   Defaults to "c128".
 
         Raises:
             ValueError: If inputs do not match available options
@@ -45,14 +49,14 @@ class FftPlanner:
         else:
             raise ValueError(f"Direction must be either `forward` or `inverse`; received `{direction}`")
 
-        if dtype.lower() == "c32":
+        if dtype.lower() == "c64":
             self._inner = _FftPlannerF32(len, d)
             self._dtype = complex64  # 64 bits total
-        elif dtype.lower() == "c64":
+        elif dtype.lower() == "c128":
             self._inner = _FftPlannerF64(len, d)
             self._dtype = complex128  # 128 bits total
         else:
-            raise ValueError(f"dtype must be either `c32` or `c64`; received `{dtype}`")
+            raise ValueError(f"dtype must be either `c64` or `c128`; received `{dtype}`")
 
     def process(self, buffer: NDArray[complex64] | NDArray[complex128]) -> NDArray:
         """Run the FFT, converting the values in the buffer in-place if possible,
@@ -72,3 +76,6 @@ class FftPlanner:
         self._inner.process(buffer_maybe_new)
 
         return buffer_maybe_new
+
+    def __call__(self, buffer: NDArray[complex64] | NDArray[complex128]) -> NDArray:
+        return self.process(buffer)
